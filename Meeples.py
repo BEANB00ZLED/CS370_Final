c700b1e87be7194610dfb4d35459c6a7d1441299
Adrian <ramdhaa@sunypoly.edu>
2023-09-18 13:06:36 -0400

    Start

---
+++ Meeples.py
@@ -0,0 +1,7 @@
+#Creates meeples figures that can be placed on tile
+class Meeple:
+        def __init__(self,x,y,width,height):
+                self.x = x
+                self.y = y
+                self.width = width
+                
