124eb9380b6621d943a9028092fe6d0896ea2980
Anthony Altieri <altiera@sunypoly.edu>
2023-09-18 18:31:59 -0400

    Can move and rotate tiles with mouse and keyboard

--- tile.py
+++ tile.py
@@ -3,6 +3,9 @@
 import pygame
 
 class Tile():
+    #Static variable
+    is_picked_up = False
+    
     def __init__(self, x, y, image_path):
         self.x = x
         self.y = y
@@ -12,20 +15,34 @@ class Tile():
         self.rect = self.image.get_rect(topleft=(self.x, self.y))
         
     def process(self, screen, event_list):
-        #End updating everything
-        screen.blit(self.image, (self.x, self.y))
-        
-        mouse_motion = None
-        
-        for event in event_list:    
-            if event.type == pygame.MOUSEMOTION:
-                mouse_motion = event.rel           
+        for event in event_list:          
+            #Looking for pressing left click
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1 and self.rect.collidepoint(event.pos):
-                    if mouse_motion != None:
-                        self.rect.move_ip(mouse_motion[0], mouse_motion[1])
-                        self.x += mouse_motion[0]
-                        self.y += mouse_motion[1]
+                    Tile.is_picked_up = True
+            #Looking for releasing left click
+            elif event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1 and self.rect.collidepoint(event.pos):
+                    Tile.is_picked_up = False
+            elif event.type == pygame.KEYDOWN:
+                #Rotates 90 degrees clockwise if e is pressed
+                if event.key == pygame.K_e:
+                    self.image = pygame.transform.rotate(self.image, -90)
+                #Rotates 90 degrees counter clockwise if q is pressed
+                elif event.key == pygame.K_q:
+                    self.image = pygame.transform.rotate(self.image, 90)
+                    
+            #Looking for mouse movement    
+            if event.type == pygame.MOUSEMOTION and Tile.is_picked_up:
+                self.rect.move_ip(event.rel)
+                self.x = self.rect.x
+                self.y = self.rect.y
+                
+                    
+                    
+        #Attatch tile to screen
+        screen.blit(self.image, (self.x, self.y))
+        
                     
 
                 
